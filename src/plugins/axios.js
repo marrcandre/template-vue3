1b92d5912da82b409c748e6fe58ac0bb56a0d12c
Marco André Mendes <marcoandre@gmail.com>
2026-02-27 15:00:49 -0300

    fix: corrigir a URL base no arquivo axios.js para localhost:8000

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Configura a baseURL usando a variável de ambiente
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:19003/api'
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL || 'http://localhost:8000/api'
 
 // axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('psg_auth_token')}`
 
